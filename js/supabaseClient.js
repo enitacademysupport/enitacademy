deffe39dec6ab200aedd8a05bf40d14493fcb945
Lucero <enitacademysupport@gmail.com>
2026-05-03 21:26:07 -0500

    Delete js/supabaseClient.js

--- js/supabaseClient.js
+++
@@ -1,4 +0,0 @@
-const supabaseUrl = "https://ugjgpirjcmyjfsgspajd.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnamdwaXJqY215amZzZ3NwYWpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NDI2MDAsImV4cCI6MjA5MzQxODYwMH0.OuERi9MUuFbxnC2Jvj_bjO4W90FbHagBKmvPSsd6Hm4";
-
-window.supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
