b7f213eb20ca51976bf1e5944f6188430608c881
Lucero <enitacademysupport@gmail.com>
2026-05-03 21:20:00 -0500

    Update registro.js

--- js/registro.js
+++ js/registro.js
@@ -1,5 +1,5 @@
-const supabaseUrl = "https://ugjgpirjcmyjfsgspajd.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVnamdwaXJqY215amZzZ3NwYWpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NDI2MDAsImV4cCI6MjA5MzQxODYwMH0.OuERi9MUuFbxnC2Jvj_bjO4W90FbHagBKmvPSsd6Hm4";
+const supabaseUrl = "...";
+const supabaseKey = "...";
 
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
