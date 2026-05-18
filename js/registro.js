da17340396c0edea90f107f04edaa87b8432b238
Lucero <enitacademysupport@gmail.com>
2026-05-18 00:24:55 -0500

    Update registro.js

--- js/registro.js
+++ js/registro.js
@@ -51,7 +51,7 @@ window.addEventListener("DOMContentLoaded", () => {
         }
 
         alert("Registro exitoso");
-        window.location.href = "../index.html";
+        window.location.href = "../paginas/dashboard.html";
     });
 
 });
